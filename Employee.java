73bac75c35a7d79425f9bd5491ac9e3e846ed9a8
Juan Pablo <36493244+juanpax@users.noreply.github.com>
2021-10-08 16:51:57 -0600

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,36 @@
+	package com.mycompany.app;
+	
+	public class Employee {
+	    public String id;
+	    public String name;
+	    public String age;
+	    public String gender ;
+	
+	    public String getId() {
+	        return id;
+	    }
+	    
+	    public String getName() {
+	        return name;
+	    }
+	    
+	    public String getAge() {
+	        return age;
+	    }
+	    
+	    public String getGender() {
+	        return gender;
+	    }
+	    
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+	    
+	    public void setAge(String age) {
+	        this.age = age;
+	    }
+	    
+	    public void setGender(String gender) {
+	        this.gender = gender;
+	    }
+	}
